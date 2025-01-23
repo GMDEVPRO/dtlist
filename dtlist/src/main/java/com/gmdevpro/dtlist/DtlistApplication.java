86d8e0ae50fe0838c2540b2dae9278d8aac4c46f
guy djoumessi <gmdnegocio@gmail.com>
2025-01-23 16:32:33 -0300

    Initial commit

---
+++ dtlist/src/main/java/com/gmdevpro/dtlist/DtlistApplication.java
@@ -0,0 +1,13 @@
+package com.gmdevpro.dtlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtlistApplication.class, args);
+	}
+
+}
---
+++ dtlist/src/test/java/com/gmdevpro/dtlist/DtlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmdevpro.dtlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
